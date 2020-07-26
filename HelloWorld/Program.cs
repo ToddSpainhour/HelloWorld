ada1aace965700484135a5754a341307e0646652
Todd Spainhour <toddspainhour@gmail.com>
2020-07-26 17:32:18 -0500

    added new requirements described in the Console Input assignment.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,10 +9,19 @@ namespace HelloWorld
         {
 
             Console.WriteLine("Welcome to Todd's C# Hello World...with a twist.");
+            Console.WriteLine("");
+
+            Console.WriteLine("First things first. What's your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine($"Well, hello there {userName}. Thanks for stopping by today.");
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
+            Console.WriteLine("");
 
-            Console.WriteLine("First we must pick a dialect in which to experience this console app.");
+            Console.WriteLine("Now lets pick a dialect in which to experience this console app.");
+            Console.WriteLine("");
             Console.WriteLine("Your options are: Southern, Northern, or Cajun");
             Console.WriteLine("Type in your answer and press Enter");
 
@@ -34,18 +43,32 @@ namespace HelloWorld
                     break;
             }
 
-            Console.ReadKey(true);
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true); //  have to press button twice to advance;
+            Console.WriteLine("");
+
+
+            Console.WriteLine($"Great work, {userName}. Now what's your favorite color?");
+            var usersFavoriteColor = Console.ReadLine();
+            Console.WriteLine("");
+
 
             var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Dog"};
 
             foreach (var animal in animals)
             {
+                Console.WriteLine("");
+                Console.WriteLine($"Have you ever seen a {usersFavoriteColor} {animal}, {userName}?");
                 var numberOfVowels = animal.Count("aeiouAEIOU".Contains);
                 var numberOfSyllables = numberOfVowels;
 
                     if (numberOfSyllables >= 2)
                     {
-                        Console.WriteLine($"{animal} has {numberOfSyllables} syllables.");
+                        Console.WriteLine($"In case you were wondering, a {animal} has {numberOfSyllables} syllables.");
+                        Console.WriteLine("");
+
                     }
             }
         }
